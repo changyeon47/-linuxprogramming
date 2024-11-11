d1110ca5f69c4de996abe8292fd5e808c365d71c
changyeon47 <jms775845@gmail.com>
2024-11-11 21:00:36 +0900

    Update main.cpp

--- 8-2장/num3/main.cpp
+++ 8-2장/num3/main.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include "shape.hpp"
-using namespace std;
-int main() {
-    Circle x(0,0,2);
-    Rect y(1,1,5,10);
-    Triangle z(2,2,5,10);
-    x.show();
-    y.show();
-    z.show();
+#include <iostream> //헤더파일 포함
+#include "shape.hpp" //헤더파일 포함
+using namespace std; //std생략
+int main() {//메인함수 정의
+    Circle x(0,0,2);//객체 생성
+    Rect y(1,1,5,10);//객체 생성
+    Triangle z(2,2,5,10);//객체 생성
+    x.show();//맴버함수 호출
+    y.show();//맴버함수 호출
+    z.show();//맴버함수 호출
 }
