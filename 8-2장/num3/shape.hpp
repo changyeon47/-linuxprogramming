6cf919390a9e3869dbf15e668ad04615d64dae05
changyeon47 <jms775845@gmail.com>
2024-11-11 21:09:21 +0900

    Update shape.hpp

--- 8-2장/num3/shape.hpp
+++ 8-2장/num3/shape.hpp
@@ -1,31 +1,31 @@
-#ifndef SHAPE_HPP
-#define SHAPE_HPP
-class Shape{
-    int x,y;
-public:
-    Shape(int x=0,int y=0);
-    ~Shape();
-    void showpositive();
+#ifndef SHAPE_HPP//조건부 컴파일 지시자 정의
+#define SHAPE_HPP//매크로 상수 선언
+class Shape{//클래스 선언
+    int x,y;//맴버변수 선언
+public://접근지정자
+    Shape(int x=0,int y=0);//생성자 선언
+    ~Shape();//소멸자 선언
+    void showpositive();//맴버함수 선언
 };
-class Circle:public Shape{
-    int radius;
-public:
-    Circle(int x,int y,int radius);
-    ~Circle();
-    void show();
+class Circle:public Shape{//클래스 상속 및 선언
+    int radius;//맴버변수 선언
+public://접근지정자
+    Circle(int x,int y,int radius);//생성자 선언
+    ~Circle();//소멸자 선언
+    void show();//맴버함수 선언
 };
-class Rect:public Shape{
-    int width,height;
-public:
-    Rect(int x,int y,int width,int height);
-    ~Rect();
-    void show();
+class Rect:public Shape{//클래스 상속 및 선언
+    int width,height;//맴버변수 선언
+public://접근지정자
+    Rect(int x,int y,int width,int height);//생성자 선언
+    ~Rect();//소멸자 선언
+    void show();//맴버함수 선언
 };
-class Triangle:public Shape{
-    int width,height;
-public:
-    Triangle(int x,int y,int width,int height);
-    ~Triangle();
-    void show();
+class Triangle:public Shape{//클래스 상속 및 선언
+    int width,height;//맴버변수 선언
+public://접근지정자
+    Triangle(int x,int y,int width,int height);//생성자 선언
+    ~Triangle();//소멸자 선언
+    void show();//맴버함수 선언
 };
-#endif
+#endif//조건부 컴파일 지시자 끝
